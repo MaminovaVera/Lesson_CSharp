392033918647aa5bf4db7a67f74eb2415a15727d
VeraMaminova <maminovavv@yandex.ru>
2022-08-08 23:40:05 +0300

    HomeWork1

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт,
+// какое число большее, а какое меньшее.
+int Prompt(string message)
+{
+    System.Console.Write(message);          // Вывести сообщение
+    string readValue = Console.ReadLine();  // Считавает с консоли строку
+    int result = int.Parse(readValue);      // Преобрадует строку в целое число
+    return result;                          // Возвращает результат
+}
+int number1;
+int number2;
+number1 = Prompt("Введите первое число ");
+number2 = Prompt("Введите второе число ");
+if (number1 > number2){
+    System.Console.WriteLine(number1);
+}
+else{
+System.Console.WriteLine(number2);
+}
+
+
+
---
+++ Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт 
+//максимальное из этих чисел.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);          // Вывести сообщение
+    string readValue = Console.ReadLine();  // Считавает с консоли строку
+    int result = int.Parse(readValue);      // Преобрадует строку в целое число
+    return result;                          // Возвращает результат
+}
+int number1;
+int number2;
+int number3;
+number1 = Prompt("Введите первое число ");
+number2 = Prompt("Введите второе число ");
+number3 = Prompt("Введите третье число ");
+
+int max = number1;
+if (number1 > max) max = number1;
+if (number2 > max) max = number2;
+if (number3 > max) max = number3;
+
+System.Console.WriteLine(max);
